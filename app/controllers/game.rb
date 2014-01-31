c375d808fb0d622a31375658a22cad7021477133
Apprentice <apprentice@dbc-chicago-6.local>
2014-01-31 16:40:08 -0600

    show card is now working

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -18,21 +18,23 @@ end
 
 post '/card' do
   @card = Card.find(params[:id].to_i)
+  @deck = Deck.find_by_name(params[:name])
   if params[:guess] == @card.answer
     @guess = "Correct!"
   else
     @guess = "Incorrect!"
   end
-  # binding.pry
   erb :card
 end
 
+post '/next' do
+  redirect "game/#{params[:name]}/#{params[:id]+1}"
+end
 
 
 
 get '/game/:deck_name/:card_id' do
   @deck = Deck.find_by_name(params[:deck_name])
-  # binding.pry
   @card = @deck.cards.find_by_id(params[:card_id].to_i)
   erb :card
 end
