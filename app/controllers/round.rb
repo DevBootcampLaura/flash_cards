a64b97ea738627b6731f73b9e76c5e44741c7a7f
Apprentice <apprentice@dbc-chicago-5.local>
2014-02-02 15:36:20 -0600

    added delete round feature when looking at round records

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -1,4 +1,4 @@
-get '/newgame' do 
+get '/newgame' do
   erb :deck_select
 end
 
@@ -7,8 +7,13 @@ get '/deck/:name' do
   Round.create(user_id: session[:id], deck_id: Deck.where(name: params[:name]).first.id, name: session[:round_name], complete: false)
 end
 
-get '/result/:name' do 
+get '/result/:name' do
   @name = params[:name]
   @id = Round.where(name: @name).first.id
   erb :result
+end
+
+get '/delete/:round' do
+  Round.destroy(params[:round])
+  redirect '/'
 end
